ec5ae9bef97063516329dd91f660fc3ae20dad91
Isabel Felicio <111832121+Isabelcf@users.noreply.github.com>
2023-08-28 23:35:55 -0300

    Add files via upload

---
+++ 11_AptoaDirigir.js
@@ -0,0 +1,9 @@
+var ano_atual = 2023
+var ano_nascimento = 2002
+var idade = (ano_atual - ano_nascimento)
+
+if (idade >= 18) {
+    console.log(`Você tem ${idade} anos, portanto pode tirar sua carteira de motorista`)    
+} else {
+    console.log(`Você tem ${idade} anos, portanto não pode tirar sua carteira de motorista `)
+}
